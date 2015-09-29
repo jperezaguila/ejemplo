b0990580b924bdb07a7ea757498d83f2b9ab3539
PRUEBA2 <jperezaguila@yaho.es>
2015-09-29 13:08:36 +0200

    29-09

--- ejemplo/fuciones.js
+++ ejemplo/fuciones.js
@@ -7,8 +7,21 @@ function saludando(texto) {
 }
 
 document.getElementById("btn1").onclick = saludo;
-document.getElementById("btn2").onclick = saludando;
+//document.getElementById("btn2").onclick = saludando;
 
 //document.getElementById("btn1").onclick = function (e) {
 //    alert("Hola " + e.target.outerText);
-}
+//}
+
+
+//document.getElementById("btn2").onclick = function () {
+//    saludando("Hola mundo es el boton2");
+//}
+
+//ejemplo de sintaxis de funcion auto invocada//
+(function () {
+    document.getElementById("btn2").onclick = function () {
+        saludando("Hola mundo es el boton2");
+    };
+    })();
+
---
+++ ejemplo/funcionescalculadora.js
@@ -0,0 +1,19 @@
+﻿var numero1 = parseInt
+var numero2= parseInt("num2");
+
+function operacion()
+{
+    //var n = document.getElementById();
+    
+    document.getElementById("btsum").onclick;
+    if document.getElementById("btnsum").onclick= function sumar();
+
+    }
+
+function sumar (){
+        for (document.getElementById) = ("btntot");
+        var resultado = ("numero1")+("numero2");
+        document.write=("resultado");
+
+    }
+
