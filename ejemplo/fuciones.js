f795dbe277b316c5b217440c7074a10b89554a16
PRUEBA2 <jperezaguila@yaho.es>
2015-09-29 10:06:56 +0200

    Ejercicio1

---
+++ ejemplo/fuciones.js
@@ -0,0 +1,14 @@
+﻿var saludo = function (texto){
+    alert ("Hola " + texto);
+
+}
+function saludando(texto) {
+    alert("HOLA " + texto.target.outerText);
+}
+
+document.getElementById("btn1").onclick = saludo;
+document.getElementById("btn2").onclick = saludando;
+
+//document.getElementById("btn1").onclick = function (e) {
+//    alert("Hola " + e.target.outerText);
+}
